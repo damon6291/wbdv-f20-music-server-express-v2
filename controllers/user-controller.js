a00c18dce636edc9f18afb8deb0f384d01a7f15f
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-12-09 16:47:45 -0500

    cookie error

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,13 +2,11 @@ const User = require('../models/user');
 const session = require('express-session');
 
 module.exports = (app) => {
-  app.set('trust proxy', 1);
   app.use(
     session({
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: true,
-      cookie: { secure: true },
     })
   );
 
