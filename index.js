ca75df10b189f89f868ac052ea97d6cc40406955
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-12-09 14:40:16 -0500

    change origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://wbdv-f20-music.herokuapp.com/');
+  res.header('Access-Control-Allow-Origin', 'https://wbdv-f20-music.herokuapp.com');
   res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true');
